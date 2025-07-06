4e6cb8e4b06d1f82c4a03d2b6f1dfdc2c02d96b0
pluto476526 <ngugikibuka99@gmail.com>
2025-07-06 14:44:18 +0300

    auto-commit [2025-07-06 14:44:18]

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,19 @@
+-- 1. Total number of bookings made by each user
+SELECT 
+    user_id,
+    COUNT(*) AS total_bookings
+FROM 
+    bookings
+GROUP BY 
+    user_id;
+
+-- 2. Rank properties based on total number of bookings using RANK window function
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM 
+    bookings
+GROUP BY 
+    property_id;
+

f058e824b9d3566fe891c30e65bce9927819832c
pluto476526 <ngugikibuka99@gmail.com>
2025-07-06 14:48:11 +0300

    auto-commit [2025-07-06 14:48:11]

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,8 @@ GROUP BY
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_booking,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number_booking
 FROM 
     bookings
 GROUP BY 

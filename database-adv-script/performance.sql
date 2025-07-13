9cdfc37c0f3a7b01f365ab3db7501c2819b59c00
pluto476526 <ngugikibuka99@gmail.com>
2025-07-13 08:54:28 +0300

    auto-commit [2025-07-13 08:54:28]

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,4 +1,4 @@
--- Initial unoptimized query: joins bookings with users, properties, and payments
+-- Initial Query: Retrieve all bookings with user, property, and payment details
 
 SELECT 
     b.booking_id,
@@ -23,28 +23,27 @@ JOIN
 LEFT JOIN 
     payments pay ON b.booking_id = pay.booking_id;
 
--- Analyze performance
 EXPLAIN ANALYZE
 SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    u.email,
-    p.property_id,
-    p.title,
-    p.location,
-    pay.payment_id,
-    pay.amount,
-    pay.status
+     b.booking_id,
+     b.start_date,
+     b.end_date,
+     u.user_id,
+     u.first_name,
+     u.last_name,
+     u.email,
+     p.property_id,
+     p.title,
+     p.location,
+     pay.payment_id,
+     pay.amount,
+     pay.status
 FROM 
-    bookings b
+     bookings b
 JOIN 
-    users u ON b.user_id = u.user_id
+     users u ON b.user_id = u.user_id
 JOIN 
-    properties p ON b.property_id = p.property_id
+     properties p ON b.property_id = p.property_id
 LEFT JOIN 
-    payments pay ON b.booking_id = pay.booking_id;
+     payments pay ON b.booking_id = pay.booking_id;
 

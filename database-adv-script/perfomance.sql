551ac3fe0dd69af336ceae8b17026cb751f4c2d3
pluto476526 <ngugikibuka99@gmail.com>
2025-07-13 08:58:04 +0300

    auto-commit [2025-07-13 08:58:04]

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,7 @@
 -- Initial Query: Retrieve all bookings with user, property, and payment details
+-- Filter: Only completed bookings after January 1, 2024
 
+EXPLAIN ANALYZE
 SELECT 
     b.booking_id,
     b.start_date,
@@ -21,29 +23,10 @@ JOIN
 JOIN 
     properties p ON b.property_id = p.property_id
 LEFT JOIN 
-    payments pay ON b.booking_id = pay.booking_id;
+    payments pay ON b.booking_id = pay.booking_id
+WHERE 
+    b.status = 'completed'
+    AND b.start_date >= '2024-01-01';
 
-EXPLAIN ANALYZE
-SELECT 
-     b.booking_id,
-     b.start_date,
-     b.end_date,
-     u.user_id,
-     u.first_name,
-     u.last_name,
-     u.email,
-     p.property_id,
-     p.title,
-     p.location,
-     pay.payment_id,
-     pay.amount,
-     pay.status
-FROM 
-     bookings b
-JOIN 
-     users u ON b.user_id = u.user_id
-JOIN 
-     properties p ON b.property_id = p.property_id
-LEFT JOIN 
-     payments pay ON b.booking_id = pay.booking_id;
+
 
